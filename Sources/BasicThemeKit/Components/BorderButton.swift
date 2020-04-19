0ce0f4eb4f076f1e8258dbd081660b11ac069a50
Jean-Marc Boullianne <jboullianne@gmail.com>
2020-04-19 15:44:55 -0500

    Added BorderButton Component.

---
+++ Sources/BasicThemeKit/Components/BorderButton.swift
@@ -0,0 +1,28 @@
+//
+//  BorderButton.swift
+//  
+//
+//  Created by Jean-Marc Boullianne on 4/19/20.
+//
+
+import Foundation
+import SwiftUI
+
+public struct BorderButton: ViewModifier {
+    public func body(content: Content) -> some View {
+        content
+            .foregroundColor(Colors.textParagraph)
+            .padding(.vertical, Sizes.paddingButtonVertical)
+            .padding(.horizontal, Sizes.paddingButtonHorizontal)
+            .background(Color.clear)
+            .cornerRadius(Sizes.cornerRadius)
+            .border(Colors.buttonPrimaryBackground.opacity(0.18), width: 2)
+            .shadow(color: Colors.shadowButtonPrimary, radius: 12, x: 0, y: 0)
+    }
+}
+
+extension View {
+    public func BorderButton() -> some View {
+        self.modifier(PrimaryButton())
+    }
+}
